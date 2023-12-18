bb8501df1bc6b0784fd2f7df38feaaae407d339a
Karczykowski <mkarczykowski@gmail.com>
2023-12-18 09:24:53 +0100

    Fix scene disappearance

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -5,16 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    private void Start()
-    {
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Spikes"))
-            {
-            Destroy(gameObject);
-            }
-    }
     private void DestroyPlayer()
     {
         Destroy(gameObject);
